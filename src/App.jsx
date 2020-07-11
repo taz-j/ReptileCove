b69e9bc1ba34521ae2e609d847dc6b8789f14abc
HeyitsmeTazG <HeyitsmeTazG@outlook.com>
2020-07-11 12:28:34 +1000

    Started Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/navbar"
 
 function App() {
   return (
     <div>
       <h1>Reptile Cove</h1>
+      <Navbar/>
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ class NavBar extends Component {
       <React.Fragment>
         <nav className="navbar navbar-dark bg-dark mb-3">
           <a className="navbar-brand" href="#">
-            <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
+            <h1>Navbar <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
           </a>
         </nav>
       </React.Fragment>
